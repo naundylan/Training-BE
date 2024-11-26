2ccd0e4791a651c6b7eb559034152ff6b8f2a4cb
quyen <naundylan@gmail.com>
2024-11-26 08:40:09 +0700

    update JS04

--- BÀI TẬP/BÀI TẬP JAVASCRIPT/JS04/toDoList/main.js
+++ BÀI TẬP/BÀI TẬP JAVASCRIPT/JS04/toDoList/main.js
@@ -2,3 +2,17 @@ var toDoInput = document.getElementById("todo-input");
 var toDoList = document.getElementById("todo-list");
 var addButton = document.getElementById("add-button");
 
+addButton.addEventListener("click", function() {
+    var toDoText = toDoInput.innerText;
+    if(toDoText !== "") {
+        var toDoItem = document.createElement("li");
+        toDoItem.textContent = toDoText;
+        toDoList.appendChild(toDoItem);
+        toDoInput.value = "";       
+    }
+    else {
+        alert("Hãy nhập một nhiệm vụ!");
+    }
+
+    
+});
