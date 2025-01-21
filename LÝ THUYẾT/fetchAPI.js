b479ba86e7de5ba2df862b09d6c29ee1160d8b71
quyen <naundylan@gmail.com>
2025-01-21 10:45:57 +0700

    update

--- LÝ THUYẾT/fetchAPI.js
+++ LÝ THUYẾT/fetchAPI.js
@@ -1,3 +1,15 @@
 fetch("https://dummyjson.com/products")
-.then(response => response.json())
-.then(data => console.log(data))
+.then(response => response.json()) // chuyển từ dạng json thành object javascript (dữ liêu thô)
+.then(data => {
+    console.log(data.products);
+
+    const newArray = data.products.map((item) => {
+        return `
+            <li>${item.title}</li>
+        `;
+    });
+    var htmls = newArray.join("")
+    var list = document.querySelector("#products-list");
+    list.innerHTML = htmls
+    
+})  // chờ chuyển dữ liệu xong thì in ra
