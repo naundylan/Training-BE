4a3587215ead368e0654d705288228514eaf56d4
quyen <naundylan@gmail.com>
2025-01-27 21:40:23 +0700

    update

--- LÝ THUYẾT/L16/index.js
+++ LÝ THUYẾT/L16/index.js
@@ -2,8 +2,11 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.set('views', './views')
+app.set('view engine', 'pug')
+
 app.get('/', (req, res) => {
-  res.send('Hello World! 126')
+  res.render('index')
 })
 
 app.listen(port, () => {
