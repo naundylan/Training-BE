08814c821826be50862a8145765478c5f9841b08
quyen <naundylan@gmail.com>
2025-02-01 19:53:12 +0700

    project init

--- THỰC HÀNH/Product-Management/index.js
+++ THỰC HÀNH/Product-Management/index.js
@@ -1,13 +1,14 @@
 const express = require('express')
+const route = require("routes/client/index.route")
 const app = express()
 const port = 3000
 
 app.set('views', './views')
 app.set('view engine', 'pug')
+app.use(express.static('public'))
 
-app.get('/', (req, res) => {
-  res.render("client\page\home\index")
-})
+// Routes
+route(app)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
