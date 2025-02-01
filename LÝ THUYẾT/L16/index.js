2ffa31b6778804e19eaa38ee509a39be8b9008a3
quyen <naundylan@gmail.com>
2025-02-01 19:16:55 +0700

    update test

--- LÝ THUYẾT/L16/index.js
+++ LÝ THUYẾT/L16/index.js
@@ -1,6 +1,16 @@
 const express = require('express')
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://127.0.0.1:27017/Test')
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error('MongoDB connection error:', err));
 const app = express()
 const port = 3000
+
+const test = mongoose.model('test', new mongoose.Schema ({
+  title : String,
+  price : Number,
+  discountPercentage : Number
+}, { collection: "test"}));
 
 app.set('views', './views')
 app.set('view engine', 'pug')
@@ -9,6 +19,15 @@ app.use(express.static('public'))
 
 app.get('/', (req, res) => {
   res.render('index')
+})
+
+app.get('/products', async (req, res) => {
+  const productList = await test.find({});
+  // console.log(productList);
+  res.render('products', {
+    title:"Danh sách sản phẩm",
+    products: productList
+  })
 })
 
 app.listen(port, () => {
