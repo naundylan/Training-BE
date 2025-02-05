bbf0de7990146057c42343886bffcaf93a42e708
quyen <naundylan@gmail.com>
2025-02-05 17:47:52 +0700

    render product page

--- THỰC HÀNH/Product-Management/controllers/client/product.controllers.js
+++ THỰC HÀNH/Product-Management/controllers/client/product.controllers.js
@@ -1,6 +1,24 @@
+const Product = require("../../models/product.models")
+
 // Product controllers
-module.exports.index = (req, res) => {
+module.exports.index = async (req, res) => {
+    // Get product list with conditions
+    const productList = await Product.find({
+        status: "active",
+        deleted: false
+    })
+
+
+    // New price
+    const newProducts = productList.map(item => {
+        item.priceNew = (item.price - item.price*item.discountPercentage/100).toFixed(0)
+        return item;
+    })
+
+
+    // Render page
     res.render("client/pages/products/index", {
-            pageTitle: "Trang danh sách sản phẩm"
+            pageTitle: "Trang danh sách sản phẩm",
+            products: newProducts
     })
 }
