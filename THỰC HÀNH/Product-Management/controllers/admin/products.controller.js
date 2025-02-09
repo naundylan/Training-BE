48c747b937a17581275197aaa404c56b104396cf
quyen <naundylan@gmail.com>
2025-02-09 21:49:40 +0700

    get data & render data

--- THỰC HÀNH/Product-Management/controllers/admin/products.controller.js
+++ THỰC HÀNH/Product-Management/controllers/admin/products.controller.js
@@ -1,6 +1,15 @@
 // Admin products controllers
-module.exports.index = (req, res) => {
+const Product = require("../../models/product.models")
+
+module.exports.index = async (req, res) => {
+    const products = await Product.find({
+        
+    })
+
+    console.log(products);
+
     res.render("admin/pages/products/index", {
-        pageTitle: "Trang danh sách sản phẩm"
+        pageTitle: "Trang danh sách sản phẩm",
+        products: products
     })
 };
